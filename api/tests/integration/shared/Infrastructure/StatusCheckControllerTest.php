a68006d9ef43bfb7f04123e39bb9206eded98afc
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2022-09-17 19:07:06 +0200

    update nginx conf to redirect all routes to php-fpm container

---
+++ api/tests/integration/shared/Infrastructure/StatusCheckControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use PHPUnit\Framework\TestCase;
+
+class StatusCheckControllerTest extends TestCase
+{
+    public function testStatusCheckReturns200()
+    {
+        $client = new Client();
+        try {
+            $response = $client->get('http://nginx/status');
+            $this->assertEquals(200, $response->getStatusCode());
+        } catch (RequestException $e) {
+            $this->hasFailed();
+        }
+    }
+}
