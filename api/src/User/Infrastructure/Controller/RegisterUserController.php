888f73a0db100b75d9a707137cdc1decf5192bc8
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2022-10-28 19:33:05 +0200

    register controller sends command to bus

--- api/src/User/Infrastructure/Controller/RegisterUserController.php
+++ api/src/User/Infrastructure/Controller/RegisterUserController.php
@@ -2,13 +2,23 @@
 
 namespace App\User\Infrastructure\Controller;
 
+use App\User\Application\Command\RegisterUserCommand;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class RegisterUserController
 {
+    public function __construct(private readonly MessageBusInterface $bus)
+    {
+    }
+
     public function __invoke(Request $request): Response
     {
-        return new Response(json_encode([]));
+        $command = new RegisterUserCommand();
+
+        $this->bus->dispatch($command);
+
+        return new Response("OK", 200);
     }
 }
--- api/tests/Unit/User/Infrastructure/Controller/RegisterUserControllerTest.php
+++ api/tests/Unit/User/Infrastructure/Controller/RegisterUserControllerTest.php
@@ -2,20 +2,44 @@
 
 namespace Test\Unit\User\Infrastructure\Controller;
 
+use App\User\Application\Command\RegisterUserCommand;
 use App\User\Infrastructure\Controller\RegisterUserController;
 use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class RegisterUserControllerTest extends TestCase
 {
-    public function test_it_returns_a_response()
+    use ProphecyTrait;
+
+    private $bus;
+
+    public function setUp(): void
     {
-        $controller = new RegisterUserController();
+        parent::setUp();
+        $this->bus = $this->prophesize(MessageBusInterface::class);
+    }
+
+    public function test_it_returns_200_response(): void
+    {
+        $command = new RegisterUserCommand();
+        $this->bus->dispatch($command)
+            ->shouldBeCalled()
+            ->willReturn(new Envelope(new RegisterUserCommand()));
+        $controller = $this->buildController();
         $request = new Request();
 
         $response = $controller($request);
 
-        $this->assertInstanceOf(Response::class, $response);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    private function buildController(): RegisterUserController
+    {
+        return new RegisterUserController(
+            $this->bus->reveal()
+        );
     }
 }
