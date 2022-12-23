9a9e0697c50814ba24518a4ca3c87fbabb40a97e
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2022-12-23 18:35:04 +0100

    use production specific nginx config file

--- user_data.sh
+++ user_data.sh
@@ -5,6 +5,9 @@
 echo "SETTING UP CONTABLE BACKEND"
 
 sudo apt update
-sudo apt install -y nginx-core
+sudo apt install -y nginx-core \
+  php8.1-fpm
 
+wget https://raw.githubusercontent.com/andreuramos/accounting/master/ops/prod.conf
+sudo mv prod.conf /etc/nginx/sites-available/default
 sudo service nginx start
