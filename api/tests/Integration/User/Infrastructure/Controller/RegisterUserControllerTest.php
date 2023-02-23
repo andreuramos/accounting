5cb079640ef4c8b56b69346b905532586e4a3789
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2023-02-23 09:36:04 +0100

    use container in integration test

--- api/tests/Integration/User/Infrastructure/Controller/RegisterUserControllerTest.php
+++ api/tests/Integration/User/Infrastructure/Controller/RegisterUserControllerTest.php
@@ -2,16 +2,20 @@
 
 namespace Test\Integration\User\Infrastructure\Controller;
 
+use App\Shared\Infrastructure\ContainerFactory;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 use PHPUnit\Framework\TestCase;
 
 class RegisterUserControllerTest extends TestCase
 {
+    const SUCCESS_EMAIL = 'noexisting@email.com';
+
     public function setUp(): void
     {
         parent::setUp();
         $this->client = new Client();
+        $this->container = ContainerFactory::create();
     }
 
     public function test_registers_a_user()
@@ -19,7 +23,7 @@ class RegisterUserControllerTest extends TestCase
         $response = $this->client->request('POST','http://nginx/register', [
             'body' => json_encode([
                 'name' => 'some name',
-                'email' => 'noexisting@email.com',
+                'email' => self::SUCCESS_EMAIL,
                 'password' => 'encodedPassword',
             ], JSON_THROW_ON_ERROR)
         ]);
