9cc82c1a6bad019c1f170d8085138d6701162324
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2023-02-23 08:37:14 +0100

    test

--- user_data.sh
+++ user_data.sh
@@ -6,7 +6,15 @@ echo "SETTING UP CONTABLE BACKEND"
 
 sudo apt update
 sudo apt install -y nginx-core \
-  php8.1-fpm
+  php8.1-fpm \
+  rbenv
+
+rbenv install 2.7.1
+rbenv global 2.7.1
+
+wget https://aws-codedeploy-eu-west-1.s3.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
 
 wget https://raw.githubusercontent.com/andreuramos/accounting/master/ops/prod.conf
 sudo mv prod.conf /etc/nginx/sites-available/default
