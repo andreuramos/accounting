77e2dc33cca963267c73059b0d21eb03932577d0
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2023-03-07 17:42:11 +0100

    build pdo from env variables

--- api/config/definitions/objects.php
+++ api/config/definitions/objects.php
@@ -1,9 +1,13 @@
 <?php
 
-$dbHost = 'mysql';
-$dbName = 'accounting-db';
-$dbUsername = 'accounting';
-$dbPassword = 'accpwd';
+require_once __DIR__ . '/../../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__.'/../..');
+$dotenv->load();
+
+$dbHost = env('DB_HOST');
+$dbName = env('DB_NAME');
+$dbUsername = env('DB_USER');
+$dbPassword = env('DB_PWD');
 
 return [
     PDO::class => new PDO(
