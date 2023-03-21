f2a0af037e5cfe7937d24d55fb4c7237b4962686
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2023-03-21 09:18:09 +0100

    test without http

--- api/tests/Integration/Shared/Infrastructure/StatusCheckControllerTest.php
+++ api/tests/Integration/Shared/Infrastructure/StatusCheckControllerTest.php
@@ -12,7 +12,7 @@ class StatusCheckControllerTest extends TestCase
     {
         $client = new Client();
         try {
-            $response = $client->get('http://accounting_nginx_1/status');
+            $response = $client->get('nginx/status');
             $this->assertEquals(200, $response->getStatusCode());
         } catch (RequestException $e) {
             $this->fail($e->getMessage());
