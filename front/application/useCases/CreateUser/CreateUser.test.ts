ff96487126fc904de6d72cd4476c6019f49b99d0
Jordi Martí <jordimarti@multiplica.com>
2023-04-04 11:48:54 -0500

    login use case

--- front/application/useCases/CreateUser/CreateUser.test.ts
+++ front/application/useCases/CreateUser/CreateUser.test.ts
@@ -1,14 +1,14 @@
 import { describe, expect, test } from 'vitest'
 import { mock } from 'vitest-mock-extended'
 import UserRepository from '@domain/UserRepository'
-import { CreateUser } from './CreateUser'
+import { CreateUserUseCase } from './CreateUserUseCase'
 
 
 describe(`CreateUser use case`, () => {
     test(`calls repo's add method.`, async () => {
         const repo = mock<UserRepository>()
         const theUser = { email: 'foo@bar.com', password: '1234' }
-        const createUser = new CreateUser(repo)
+        const createUser = new CreateUserUseCase(repo)
 
         await createUser.execute(theUser)
 
@@ -19,7 +19,7 @@ describe(`CreateUser use case`, () => {
         const repo = mock<UserRepository>()
         repo.exists.mockReturnValue(Promise.resolve(true))
         const theUser = { email: 'foo@bar.com', password: '1234' }
-        const createUser = new CreateUser(repo)
+        const createUser = new CreateUserUseCase(repo)
 
         const result = await createUser.execute(theUser)
 
@@ -28,10 +28,10 @@ describe(`CreateUser use case`, () => {
 
     test(`returns error when email is invalid.`, async () => {
         const repo = mock<UserRepository>()
-        const theUser = { email: 'invalidEmail', password: '1234' }
-        const createUser = new CreateUser(repo)
+        const wrongUser = { email: 'invalidEmail', password: '1234' }
+        const createUser = new CreateUserUseCase(repo)
 
-        const result = await createUser.execute(theUser)
+        const result = await createUser.execute(wrongUser)
 
         expect(result.value.errorValue().message).toBe('Email is not valid.')
     })
@@ -39,7 +39,7 @@ describe(`CreateUser use case`, () => {
     test('returns no error when user is created.', async () => {
         const repo = mock<UserRepository>()
         const theUser = { email: 'foo@bar.com', password: '1234' }
-        const createUser = new CreateUser(repo)
+        const createUser = new CreateUserUseCase(repo)
 
         const result = await createUser.execute(theUser)
 
