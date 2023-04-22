9a57816b3a57467e526a8465c191b13a228d16cd
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2023-04-23 00:57:32 +0200

    token must be from an existing user

--- api/src/User/Application/UseCase/RefreshTokensUseCase.php
+++ api/src/User/Application/UseCase/RefreshTokensUseCase.php
@@ -5,24 +5,44 @@ namespace App\User\Application\UseCase;
 use App\User\Application\Auth\AuthTokenDecoderInterface;
 use App\User\Application\Command\RefreshTokensCommand;
 use App\User\Application\Result\LoginResult;
+use App\User\Domain\Entity\User;
 use App\User\Domain\Exception\InvalidAuthToken;
 use App\User\Domain\Exception\InvalidCredentialsException;
+use App\User\Domain\Model\UserRepositoryInterface;
+use App\User\Domain\ValueObject\Email;
 
 class RefreshTokensUseCase
 {
     public function __construct(
-        private readonly AuthTokenDecoderInterface $tokenDecoder
+        private readonly AuthTokenDecoderInterface $tokenDecoder,
+        private readonly UserRepositoryInterface $userRepository
     ) {
     }
 
     public function __invoke(RefreshTokensCommand $command): LoginResult
     {
         try {
-            ($this->tokenDecoder)($command->refreshToken);
+            $tokenPayload = ($this->tokenDecoder)($command->refreshToken);
+            if (!isset($tokenPayload['user']) || !isset($tokenPayload['expiration'])) {
+                throw new InvalidAuthToken();
+            }
         } catch (InvalidAuthToken $exception) {
             throw new InvalidCredentialsException();
         }
 
+        $user = $this->getUser($tokenPayload);
+
         throw new InvalidCredentialsException();
+    }
+
+    private function getUser(array $tokenPayload): User
+    {
+        $user = $this->userRepository->getByEmail(
+            new Email($tokenPayload['user'])
+        );
+        if (null === $user) {
+            throw new InvalidCredentialsException();
+        }
+        return $user;
     }
 }
--- api/tests/Unit/User/Application/UseCase/RefreshTokensUseCaseTest.php
+++ api/tests/Unit/User/Application/UseCase/RefreshTokensUseCaseTest.php
@@ -7,6 +7,8 @@ use App\User\Application\Command\RefreshTokensCommand;
 use App\User\Application\UseCase\RefreshTokensUseCase;
 use App\User\Domain\Exception\InvalidAuthToken;
 use App\User\Domain\Exception\InvalidCredentialsException;
+use App\User\Domain\Model\UserRepositoryInterface;
+use App\User\Domain\ValueObject\Email;
 use PHPUnit\Framework\TestCase;
 use Prophecy\PhpUnit\ProphecyTrait;
 
@@ -15,21 +17,47 @@ class RefreshTokensUseCaseTest extends TestCase
     use ProphecyTrait;
 
     private $tokenDecoder;
+    private $userRepository;
 
     public function setUp(): void
     {
         parent::setUp();
         $this->tokenDecoder = $this->prophesize(AuthTokenDecoderInterface::class);
+        $this->userRepository = $this->prophesize(UserRepositoryInterface::class);
     }
 
     public function test_invalid_token_throws_exception()
     {
         $command = new RefreshTokensCommand("im.really.invalid");
         $this->tokenDecoder->__invoke("im.really.invalid")->willThrow(InvalidAuthToken::class);
-        $useCase = new RefreshTokensUseCase($this->tokenDecoder->reveal());
+        $useCase = $this->getUseCase();
 
         $this->expectException(InvalidCredentialsException::class);
 
         $useCase->__invoke($command);
+    }
+
+    public function test_not_found_user_throws_exception()
+    {
+        $command = new RefreshTokensCommand("jwt.wrong.payload");
+        $this->tokenDecoder->__invoke("jwt.wrong.payload")->willReturn([
+            'user' => "weird@email.com",
+            'expiration' => 123
+        ]);
+        $this->userRepository->getByEmail(new Email('weird@email.com'))
+            ->willReturn(null);
+        $useCase = $this->getUseCase();
+
+        $this->expectException(InvalidCredentialsException::class);
+
+        $useCase->__invoke($command);
+    }
+
+    private function getUseCase(): RefreshTokensUseCase
+    {
+        return new RefreshTokensUseCase(
+            $this->tokenDecoder->reveal(),
+            $this->userRepository->reveal()
+        );
     }
 }
