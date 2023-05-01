a95eda1ac021f4b7c6aeac57be510a88aaf60e0c
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2023-05-01 21:33:39 +0200

    get user expenses usecase returns repo result

--- api/src/Transaction/Application/UseCase/GetUserExpensesUseCase.php
+++ api/src/Transaction/Application/UseCase/GetUserExpensesUseCase.php
@@ -4,11 +4,19 @@ namespace App\Transaction\Application\UseCase;
 
 use App\Transaction\Application\Command\GetUserExpensesCommand;
 use App\Transaction\Application\Result\UserExpenses;
+use App\Transaction\Domain\Model\ExpenseRepositoryInterface;
 
 class GetUserExpensesUseCase
 {
+    public function __construct(
+        private readonly ExpenseRepositoryInterface $expenseRepository
+    ) {
+    }
+
     public function __invoke(GetUserExpensesCommand $command): UserExpenses
     {
-        return new UserExpenses([]);
+        $expenses = $this->expenseRepository->getByUser($command->user);
+
+        return new UserExpenses($expenses);
     }
 }
--- api/tests/Unit/Transaction/Application/UseCase/GetUserExpensesUseCaseTest.php
+++ api/tests/Unit/Transaction/Application/UseCase/GetUserExpensesUseCaseTest.php
@@ -5,7 +5,9 @@ namespace Test\Unit\Transaction\Application\UseCase;
 use App\Shared\Domain\ValueObject\Id;
 use App\Transaction\Application\Command\GetUserExpensesCommand;
 use App\Transaction\Application\UseCase\GetUserExpensesUseCase;
+use App\Transaction\Domain\Entity\Expense;
 use App\Transaction\Domain\Model\ExpenseRepositoryInterface;
+use App\Transaction\Domain\ValueObject\Money;
 use App\User\Domain\Entity\User;
 use App\User\Domain\ValueObject\Email;
 use PHPUnit\Framework\TestCase;
@@ -27,11 +29,45 @@ class GetUserExpensesUseCaseTest extends TestCase
     {
         $user = new User(new Id(1), new Email("getexpenses@usecase.app"), "");
         $this->expenseRepository->getByUser($user)->willReturn([]);
-        $useCase = new GetUserExpensesUseCase();
+        $useCase = new GetUserExpensesUseCase(
+            $this->expenseRepository->reveal()
+        );
         $command = new GetUserExpensesCommand($user);
 
         $result = $useCase($command);
 
         $this->assertCount(0, $result->expenses);
+    }
+
+    public function test_usecase_returns_same_expenses_than_repo()
+    {
+        $user = new User(new Id(1), new Email("getexpenses@usecase.app"), "");
+        $expense1 = new Expense(
+            new Id(1),
+            new Id(1),
+            new Money(200, "EUR"),
+            "expense 1",
+            new \DateTime()
+        );
+        $expense2 = new Expense(
+            new Id(2),
+            new Id(1),
+            new Money(100, "EUR"),
+            "expense 2",
+            new \DateTime()
+        );
+        $this->expenseRepository->getByUser($user)
+            ->shouldBeCalled()
+            ->willReturn([
+                $expense1, $expense2
+            ]);
+        $useCase = new GetUserExpensesUseCase(
+            $this->expenseRepository->reveal()
+        );
+        $command = new GetUserExpensesCommand($user);
+
+        $result = $useCase($command);
+
+        $this->assertCount(2, $result->expenses);
     }
 }
