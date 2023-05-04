3a91b9c011aef78b6f9e2c5c295442f3a7595390
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2023-05-04 09:16:48 +0200

    implemented mysql save

--- api/src/Transaction/Infrastructure/Repository/MysqlIncomeRepository.php
+++ api/src/Transaction/Infrastructure/Repository/MysqlIncomeRepository.php
@@ -4,12 +4,33 @@ namespace App\Transaction\Infrastructure\Repository;
 
 use App\Transaction\Domain\Entity\Income;
 use App\Transaction\Domain\Model\IncomeRepositoryInterface;
+use PDO;
 
 class MysqlIncomeRepository implements IncomeRepositoryInterface
 {
+    public function __construct(
+        private readonly PDO $pdo
+    ) {
+    }
 
     public function save(Income $income): void
     {
-        // TODO: Implement save() method.
+        $stmt = $this->pdo->prepare(
+            'INSERT INTO income ' .
+            '(user_id, amount, description, date) ' .
+            'VALUES (:user_id, :amount, :description, :date)'
+        );
+
+        $userId = $income->userId->getInt();
+        $amountCents = $income->amount->amountCents;
+        $description = $income->description;
+        $date = $income->date->format('Y-m-d');
+
+        $stmt->bindParam(':user_id', $userId);
+        $stmt->bindParam(':amount', $amountCents);
+        $stmt->bindParam(':description', $description);
+        $stmt->bindParam(':date', $date);
+
+        $stmt->execute();
     }
 }
