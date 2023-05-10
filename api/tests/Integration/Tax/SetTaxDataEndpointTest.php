fd01cf65ac8eb56947889d632ee7f58a9f70390e
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2023-05-10 11:51:19 +0200

    test set tax data returns 200

--- api/tests/Integration/Tax/SetTaxDataEndpointTest.php
+++ api/tests/Integration/Tax/SetTaxDataEndpointTest.php
@@ -6,9 +6,36 @@ use Test\Integration\EndpointTest;
 
 class SetTaxDataEndpointTest extends EndpointTest
 {
+    const EMAIL = "set@taxdata.test";
+
     public function test_unauthorized_returns_401()
     {
         $response = $this->client->post('/tax/data');
         $this->assertEquals(401, $response->getStatusCode());
+    }
+
+    public function test_correct_request_returns_200()
+    {
+        $this->registerUser(self::EMAIL, "");
+        $this->login(self::EMAIL, "");
+
+        $response = $this->client->post('/tax/data', [
+            'body' => json_encode([
+                'tax_name' => "Moixa Brewing SL",
+                'tax_number' => "B07656565",
+                'tax_address_street' => "Andreu Jaume Nadal 29",
+                'tax_address_zip_code' => "07013",
+                'tax_address_region' => "Illes Balears",
+            ], JSON_THROW_ON_ERROR),
+            'headers' => ['Authorization' => 'Bearer '.$this->authToken ]
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        $this->deleteUser(self::EMAIL);
     }
 }
