0334eb9d1499818bd4d28d3b224d5899f56c3b5e
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2023-05-14 18:31:03 +0200

    controller checks for missing params

--- api/src/Invoice/Infrastructure/Controller/CreateInvoiceController.php
+++ api/src/Invoice/Infrastructure/Controller/CreateInvoiceController.php
@@ -2,17 +2,36 @@
 
 namespace App\Invoice\Infrastructure\Controller;
 
+use App\Shared\Domain\Exception\MissingMandatoryParameterException;
 use App\Shared\Infrastructure\ApiResponse;
 use App\Shared\Infrastructure\Controller\AuthorizedController;
 use Symfony\Component\HttpFoundation\Request;
 
 class CreateInvoiceController extends AuthorizedController
 {
+    private const MANDATORY_PARAMETERS = [
+        'income_id', 'customer_name', 'customer_tax_name',
+        'customer_tax_number', //'customer_tax_address',
+        'customer_tax_zip_code'
+    ];
+
     public function __invoke(Request $request): ApiResponse
     {
         $this->auth($request);
+        $requestContent = json_decode($request->getContent(), true);
+        $this->guardMandatoryParameters($requestContent);
+
         return new ApiResponse([
             'invoice_number' => 123,
         ]);
+    }
+
+    private function guardMandatoryParameters(mixed $requestContent): void
+    {
+        foreach (self::MANDATORY_PARAMETERS as $parameter) {
+            if (!isset($requestContent[$parameter])) {
+                throw new MissingMandatoryParameterException($parameter);
+            }
+        }
     }
 }
--- api/tests/Unit/Invoice/Infrastructure/Controller/CreateInvoiceControllerTest.php
+++ api/tests/Unit/Invoice/Infrastructure/Controller/CreateInvoiceControllerTest.php
@@ -3,6 +3,7 @@
 namespace Test\Unit\Invoice\Infrastructure\Controller;
 
 use App\Invoice\Infrastructure\Controller\CreateInvoiceController;
+use App\Shared\Domain\Exception\MissingMandatoryParameterException;
 use App\User\Domain\Exception\InvalidCredentialsException;
 use Symfony\Component\HttpFoundation\Request;
 use Test\Unit\Shared\Infrastructure\Controller\AuthorizedControllerTest;
@@ -20,6 +21,22 @@ class CreateInvoiceControllerTest extends AuthorizedControllerTest
         $controller = $this->getController();
 
         $this->expectException(InvalidCredentialsException::class);
+
+        $controller($request);
+    }
+
+    public function test_fails_if_missing_parameters()
+    {
+        $request = $this->buildAuthorizedRequest([
+            "customer_name" => "Atomic Garden",
+            "customer_tax_name" => "Atomic Garden SL",
+            "customer_tax_number" => "43568953F",
+            "customer_tax_address" => "Carrer fals 123",
+            "customer_tax_zip_code" => "07014"
+        ]);
+        $controller = $this->getController();
+
+        $this->expectException(MissingMandatoryParameterException::class);
 
         $controller($request);
     }
