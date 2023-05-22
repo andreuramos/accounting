d95f99aa060151528829bafca2e4c1c2e8a7b0a3
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2023-05-22 18:14:51 +0200

    business repository getByTaxNumber implemented

--- api/src/Invoice/Infrastructure/Repository/MysqlBusinessRepository.php
+++ api/src/Invoice/Infrastructure/Repository/MysqlBusinessRepository.php
@@ -7,13 +7,43 @@ use App\Invoice\Domain\Model\BusinessRepositoryInterface;
 use App\Shared\Domain\ValueObject\Id;
 use App\Tax\Domain\Entity\TaxData;
 use App\Tax\Domain\ValueObject\Address;
+use PDO;
 
 class MysqlBusinessRepository implements BusinessRepositoryInterface
 {
+    public function __construct(private readonly PDO $PDO)
+    {
+    }
 
     public function getByTaxNumber(string $taxNumber): ?Business
     {
-        return null;
+        $stmt = $this->PDO->prepare(
+            'SELECT b.id as business_id, b.name, ' .
+            ' td.id as tax_data_id, td.* FROM business b LEFT JOIN tax_data td ' .
+            'ON b.taxDataId = td.id ' .
+            'WHERE td.tax_number = :tax_number'
+        );
+        $stmt->bindParam(':tax_number', $taxNumber);
+        $stmt->execute();
+
+        $result = $stmt->fetch();
+        if (false === $result) {
+            return null;
+        }
+
+        $taxData = new TaxData(
+            new Id($result['tax_data_id']),
+            $result['tax_name'],
+            $result['tax_number'],
+            new Address($result['address'], $result['zip_code'])
+        );
+        $business = new Business(
+            new Id($result['business_id']),
+            $result['name'],
+            $taxData
+        );
+
+        return $business;
     }
 
     public function save(Business $business): Id
