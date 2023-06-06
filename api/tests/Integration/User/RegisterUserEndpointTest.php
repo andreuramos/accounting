eb4d5ee30e17ed76a5b75afb0d889548f5c725e3
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2023-06-06 12:22:39 +0200

    test current usecase

--- api/tests/Integration/User/RegisterUserEndpointTest.php
+++ api/tests/Integration/User/RegisterUserEndpointTest.php
@@ -26,6 +26,29 @@ class RegisterUserEndpointTest extends EndpointTest
         $this->assertNotNull($user);
     }
 
+    public function test_creates_an_account_associated_to_the_user()
+    {
+        $this->markTestIncomplete();
+        $response = $this->client->request('POST', '/user', [
+            'body' => json_encode([
+                'name' => 'some name',
+                'email' => self::SUCCESS_EMAIL,
+                'password' => 'encodedPassword',
+            ], JSON_THROW_ON_ERROR)
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $user = $this->pdo
+            ->query('SELECT * FROM user WHERE email = "' . self::SUCCESS_EMAIL . '"')
+            ->fetch();
+        $this->assertArrayHasKey('account_id', $user);
+        $account = $this->pdo
+            ->query('SELECT * FROM account WHERE main_user_id = ' . $user['id'])
+            ->fetch();
+        $this->assertNotNull($account);
+        $this->assertEquals($user['account_id'], $account['id']);
+    }
+
     public function test_fails_if_no_email()
     {
         try {
---
+++ api/tests/Unit/User/Application/UseCase/RegisterUserUseCaseTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Test\Unit\User\Application\UseCase;
+
+use App\User\Application\Command\RegisterUserCommand;
+use App\User\Application\UseCase\RegisterUserUseCase;
+use App\User\Domain\Service\UserRegisterer;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Prophecy\PhpUnit\ProphecyTrait;
+
+class RegisterUserUseCaseTest extends TestCase
+{
+    use ProphecyTrait;
+
+    private $userRegisterer;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->userRegisterer = $this->prophesize(UserRegisterer::class);
+    }
+
+    public function test_user_is_registered()
+    {
+        $this->userRegisterer->execute(Argument::any())->shouldBeCalled();
+        $command = new RegisterUserCommand('my@email.com', "123");
+        $usecase = new RegisterUserUseCase($this->userRegisterer->reveal());
+
+        $usecase($command);
+    }
+}
