a2d4d40e031eee1e2a05333936b09da7e7091e50
Jordi Martí <jmartidominguez@gmail.com>
2023-11-16 16:24:54 -0500

    303 Forms calculated IVA cuota

--- front/src/views/Form303.view.test.tsx
+++ front/src/views/Form303.view.test.tsx
@@ -1,13 +1,45 @@
-import { describe, expect, test } from 'vitest'
+import { beforeEach, describe, expect, test } from 'vitest'
 import { RouterProvider, createMemoryRouter } from 'react-router-dom'
 import { routes } from 'src/routes'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
 
-describe(`303 Form view`, () => {
+const renderRouter = (url: string) => {
+    const router = createMemoryRouter(routes, { initialEntries: [url] })
+    render(<RouterProvider router={router} />)
+}
+
+describe(`303 Form view`, async () => {
     test(`Can navigate to page`, () => {
-        const router = createMemoryRouter(routes, { initialEntries: ['/303-form'] })
-        render(<RouterProvider router={router} />)
+        renderRouter('/303-form')
+
         expect(screen.getByText('Formulario 303')).toBeInTheDocument()
+    })
+
+    describe(`calculate 21% IVA of base imponible`, () => {
+        let input: HTMLInputElement
+        let calculatedCuota: HTMLInputElement
+
+        beforeEach(() => {
+            renderRouter('/303-form')
+            input = screen.getByLabelText('Base imponible')
+            calculatedCuota = screen.getByLabelText('Cuota')
+        })
+
+        test.each([
+            ['1000', '210'],
+            ['500', '105']
+        ])(`For base imponible of %i, should calculate %i`, async (base, expectedCuota) => {
+
+            const user = userEvent.setup()
+            await user.type(input, base)
+
+            expect(calculatedCuota.value).toEqual(expectedCuota)
+        })
+
+        test(`Cuota field should be read-only`, () => {
+            expect(calculatedCuota).toHaveAttribute('readOnly')
+        })
     })
 })
--- front/src/views/Form303.view.tsx
+++ front/src/views/Form303.view.tsx
@@ -1,5 +1,20 @@
+import { ChangeEvent, useState } from 'react'
+
 export const Form303View = () => {
+
+    const [cuota, setCuota] = useState(0)
+
+    const handleChangeBaseImponible = (e: ChangeEvent<HTMLInputElement>) => {
+        setCuota(parseInt(e.target.value) * 0.21)
+    }
+
     return (
-        <h1 role="heading">Formulario 303</h1>
+        <>
+            <h1>Formulario 303</h1>
+            <label htmlFor="base-imponible">Base imponible</label>
+            <input id="base-imponible" onChange={handleChangeBaseImponible} />
+            <label htmlFor="cuota">Cuota</label>
+            <input id="cuota" value={cuota} readOnly />
+        </>
     )
 }
